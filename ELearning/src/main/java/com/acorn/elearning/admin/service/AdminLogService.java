50d0a0809dd1b22b443015b444215312c98b7125
wnckd010339 <wnckd010339@gmail.com>
2026-06-30 17:35:53 +0900

    주창작업:user, log 관련 작업중

--- ELearning/src/main/java/com/acorn/elearning/admin/service/AdminLogService.java
+++ ELearning/src/main/java/com/acorn/elearning/admin/service/AdminLogService.java
@@ -1,10 +1,37 @@
 package com.acorn.elearning.admin.service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import com.acorn.elearning.admin.mapper.AdminOperationLogMapper;
+import com.acorn.elearning.admin.model.AdminOperationLog;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdminLogService {
+
+    private final AdminOperationLogMapper mapper;
+
+    public List<AdminOperationLog> findAll(){
+
+        return mapper.findAll();
+    }
+
+    public Optional<AdminOperationLog> findById(Long id){
+        return mapper.findById(id);
+    }
+
+    public int insert(AdminOperationLog model) {
+        return mapper.insert(model);
+    }
+
+    public int update(AdminOperationLog model) {
+        return mapper.update(model);
+    }
+
     public Map<String, Object> stub(String action) {
         // TODO 구현 예시입니다. 실제 parameter와 return DTO로 method signature를 교체하세요.
         // SessionUser admin = currentAdminSessionUser();
