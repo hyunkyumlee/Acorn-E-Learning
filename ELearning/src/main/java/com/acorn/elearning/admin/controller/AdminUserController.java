6eb01523809fc8d004d4505390faa4c9a811a02e
wnckd010339 <wnckd010339@gmail.com>
2026-07-02 16:18:19 +0900

    주창작업:사용자 관리 기능 추가

--- ELearning/src/main/java/com/acorn/elearning/admin/controller/AdminUserController.java
+++ ELearning/src/main/java/com/acorn/elearning/admin/controller/AdminUserController.java
@@ -1,11 +1,21 @@
 package com.acorn.elearning.admin.controller;
 
+import com.acorn.elearning.admin.dto.response.AdminUserManageRowResponse;
+import com.acorn.elearning.admin.form.SubjectForm;
+import com.acorn.elearning.admin.service.AdminUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class AdminUserController {
+
+    private final AdminUserService service;
+
 
     @GetMapping("/admin/users")
     public String users(Model model) {
@@ -14,7 +24,13 @@ public class AdminUserController {
         // AdminManagePageView view = adminUserService.users(sessionUser);
         // model.addAttribute("view", view);
         // 필요한 경우 model.addAttribute("form", new XxxForm()); 값도 같이 넣으세요.
+
+        List<AdminUserManageRowResponse> userList = service.findAll();
+        model.addAttribute("userList", userList);
+
+        model.addAttribute("userStatusForm", new SubjectForm());
+
         model.addAttribute("screen", "admin/users");
-        return "admin/users";
+        return "admin/adminUsers";
     }
 }
--- ELearning/src/main/java/com/acorn/elearning/admin/service/AdminUserService.java
+++ ELearning/src/main/java/com/acorn/elearning/admin/service/AdminUserService.java
@@ -1,13 +1,34 @@
 package com.acorn.elearning.admin.service;
 
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import com.acorn.elearning.admin.dto.response.AdminUserManageRowResponse;
+import com.acorn.elearning.admin.mapper.AdminUserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminUserService {
+
+    private final AdminUserMapper mapper;
+
+    public List<AdminUserManageRowResponse> findAll(){
+        return mapper.findAll();
+    }
+
+    public Optional<AdminUserManageRowResponse> findById(Long userId){
+        return mapper.findById(userId);
+    }
+
+    public Optional<AdminUserManageRowResponse> findLearningSummaryByUserId(Long userId){
+        return mapper.findLearningSummaryByUserId(userId);
+    }
+
+
 
     public Map<String, Object> stub(String action) {
         // TODO 구현 예시입니다. 실제 parameter와 return DTO로 method signature를 교체하세요.
