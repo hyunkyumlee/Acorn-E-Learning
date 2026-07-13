3f6b3d193eb19f0af464b462a0833634a3b333d9
wnckd010339 <wnckd010339@gmail.com>
2026-07-13 10:42:32 +0900

    주창작업:과목 비활성화 작업 진행중

--- ELearning/src/main/java/com/acorn/elearning/admin/mapper/AdminContentMapper.java
+++ ELearning/src/main/java/com/acorn/elearning/admin/mapper/AdminContentMapper.java
@@ -1,6 +1,14 @@
 package com.acorn.elearning.admin.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface AdminContentMapper {
 
-}
+    int deactivateCurriculumNodesBySubjectId(@Param("subjectId") Long subjectId);
 
+    int deactivateLessonsBySubjectId(@Param("subjectId") Long subjectId);
+
+    int deactivateProblemsBySubjectId(@Param("subjectId") Long subjectId);
+
+    int cancelActiveExamsBySubjectId(@Param("subjectId") Long subjectId);
+}
