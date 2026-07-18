7819aa58c19626d9f66c6dcc4e60d683c5f74a0b
hyunkyumlee <hg102938363736@gmail.com>
2026-07-18 17:15:04 +0900

    배포: CI 독립 테스트 profile 추가

--- ELearning/src/test/java/com/acorn/elearning/ELearningApplicationTests.java
+++ ELearning/src/test/java/com/acorn/elearning/ELearningApplicationTests.java
@@ -2,8 +2,10 @@ package com.acorn.elearning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ELearningApplicationTests {
     @Test
     void contextLoads() {
